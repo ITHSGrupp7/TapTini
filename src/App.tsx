cecdb019e23f70aabd4b2ded0292202d979b8f9d
Vilma Nilsson <vilman.nilsson@outlook.com>
2024-04-02 14:34:06 +0200

    Added folders and removed unnecessary code
    Co-authored-by: mlexander <mlexander@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { MenyCourseComponent } from './components/Data/MenyCourseComponent'
 
 function App() {
-  const [page, setPage] = useState(<MenyCourseComponent/>)
+  const [page, setPage] = useState(<div></div>)
 
   return (
     <>
       <header>
-       <img className="Logo" src="/src/assets/images/Tap&Tini Trans.png" alt='Tap&Tini Logo'></img>
-        <a href="/cart">
-          <span className="material-symbols-outlined">
-            shopping_cart
-          </span>
-        </a>
       </header>
-
       <main className='content'>
         {page}
       </main>
